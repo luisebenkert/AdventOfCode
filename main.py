21445ffe71046b5c216e852065f6c899a0b282c3
Luise Benkert <28504856+luisebenkert@users.noreply.github.com>
2021-12-01 17:55:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import math
+
+number_of_increases = -1
+previous_depth = -math.inf
+
+with open('input.txt', 'r') as file:
+  for line in file:
+    stripped_line = line.strip()
+    if not stripped_line:
+      continue
+
+    current_depth = int(stripped_line)
+
+    if current_depth > previous_depth:
+      number_of_increases += 1
+
+    previous_depth = current_depth
+
+print(number_of_increases)
